7b126ccb8fb86aaf0a915c6f76e07f529065d693
Adlet Turagaliyev <159787089+adspades@users.noreply.github.com>
2024-03-04 19:21:45 +0600

    Create 1.py

---
+++ builtin_functions/1.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+nums_list = []
+
+while True:
+    nums = input()
+    if nums == '':
+        break
+    nums_list.append(int(nums))
+
+result = str(reduce(lambda x, y : x * y, nums_list))
+
+print(f"The result of the multiplication is {result}")
